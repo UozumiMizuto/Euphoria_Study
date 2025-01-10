774a70d6cee99e45ca04d9fc638dae16017d11b9
masumi uozumi <41750783+RyutoThurusaki@users.noreply.github.com>
2025-01-10 21:25:17 +0900

    抽選機構

--- Anagram.ex
+++ Anagram.ex
@@ -1,7 +1,18 @@
--- Sample of Euphoria Source code --  
 include std/io.e 
+include std/math.e
 constant CONSOLE = 1, 
          KEYBOARD = 0, 
          $ 
+         
+
+--単語のリスト
+sequence words
+words = {"Wonder","Banana","Forest","Castle","Sunset","Uozumi"}
+
+--単語の抽選
+integer index = rand(length(words))
+sequence word = words[index]
           
-puts(1, "Hellooooo")
+--InterFace
+puts(1, "Anagram Game!!!")
+
